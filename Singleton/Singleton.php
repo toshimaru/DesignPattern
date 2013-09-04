8c2f20026b65ac3b4e6ec7ab40b40f5c9cc97cb2
toshi <me@toshimaru.net>
2013-09-03 20:30:20 -0700

    Add Singleton php file.

---
+++ Singleton/Singleton.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Singleton {
+  static private $instance = null;
+
+  function __construct() {
+  }
+
+  static public function getInstance() {
+    if (self::$instance === null)
+      self::$instance = new self();
+    return self::$instance;
+  }
+}
+
+$singleton1 = Singleton::getInstance();
+$singleton2 = Singleton::getInstance();
